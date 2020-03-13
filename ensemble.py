b097caccd2c8dbd07c4d61aac55b1d532f6578b4
GabrielAtkinson <Gabriel.atkinson14@gmail.com>
2020-03-13 14:47:07 +1100

    Ensemble

--- ensemble.py
+++ ensemble.py
@@ -1,20 +1,45 @@
-import sklearn.ensemble.VotingClassifier
-import pipeline
+from sklearn.ensemble import VotingClassifier
+from sklearn.datasets import fetch_20newsgroups
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn import svm
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from pipeline import A2Pipeline
+from sklearn.linear_model import LogisticRegression
+
+
+def build_pipeline(model = DecisionTreeClassifier(random_state=0, criterion='gini')):
+    return Pipeline(([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer(use_idf=True)),
+        ('clf', model),
+    ]))
 
 if __name__ == '__main__':
-    test_ng = fetch_20newsgroups(subset='test', remove=(['headers', 'footers', 'quotes']))
-    train_ng = fetch_20newsgroups(subset='train', remove=(['headers', 'footers', 'quotes']))
+    test_ng = fetch_20newsgroups(subset='test', remove=(['headers', 'footers', 'quotes']), download_if_missing=True, categories=None, shuffle=True, random_state=42)
+    train_ng = fetch_20newsgroups(subset='train', remove=(['headers', 'footers', 'quotes']), download_if_missing=True, categories=None, shuffle=True, random_state=42)
 
     pl = A2Pipeline()
-    grid = pl.grid_search_tune()
 
-    svc_clf = grid.fit(train_ng.data, train_ng.target)
+    svc_clf = pl.pipeline
+    dt_clf = build_pipeline(model=DecisionTreeClassifier(splitter='random',class_weight='balanced',min_samples_leaf=1,min_samples_split=50,max_depth=200))
+    lr_clf = build_pipeline(model=LogisticRegression())
+    ensemble = VotingClassifier(voting='hard', weights=None,
+            estimators=[('lr', lr_clf), ('svm', svc_clf)], n_jobs=-1)
 
-    ensemble = VotingClassifier( estimators=[('svc', svc_clf), ('dt', dt_clf), ('lr', lr_clf)}])
-
-    ensemble.fit(test_ng.data, test_ng.target)
-    pred = ensemble.predict(train_ng)
+    ensemble.fit(train_ng.data, train_ng.target)
+    pred = ensemble.predict(test_ng.data)
 
     print(classification_report(test_ng.target, pred))
-    print(np.meanpred == test_ng.target ))
+    print(np.mean(pred == test_ng.target ))
+    print(np.std(pred == test_ng.target))
+
+    train_pred = ensemble.predict(train_ng.data)
+
+    print(classification_report(train_ng.target, train_pred))
+    print(np.mean(train_pred == train_ng.target ))
+    print(np.std(train_pred == train_ng.target))
 
--- pipeline.py
+++ pipeline.py
@@ -7,6 +7,7 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import KFold
+import glob, json, random
 
 class A2Pipeline:
 
@@ -32,24 +33,77 @@ class A2Pipeline:
 
         return GridSearchCV(self.pipeline, param_grid, cv=3, verbose=True, n_jobs=-1)
 
+    @staticmethod
+    def load_imdb(sub_set='train'):
+        path_pos = './imdb/aclImdb/{}/pos/'.format(sub_set)
+        path_neg = './imdb/aclImdb/{}/neg/'.format(sub_set)
+        
+        neg_files = []
+        pos_files = []
+
+        for file in glob.glob(path_neg + "*.txt"):
+            f = open(file, "r")
+            neg_files.append(f.read())
+        for file in glob.glob(path_pos + "*.txt"):
+            f = open(file, "r")
+            pos_files.append(f.read())
+
+        neg = list(zip(neg_files, [0]*len(neg_files)))
+        pos = list(zip(neg_files, [1]*len(pos_files)))
+        sentiments = neg + pos
+
+        random.shuffle(sentiments)
+
+        dataset = {'data': [], 'target':[]}
+        for s in sentiments:
+            dataset['data'].append(s[0])
+            dataset['target'].append(s[1])
+
+        return (np.array(dataset['data']), np.array(dataset['target']))
 
 if __name__ == '__main__':
     test_ng = fetch_20newsgroups(subset='test', remove=(['headers', 'footers', 'quotes']))
     train_ng = fetch_20newsgroups(subset='train', remove=(['headers', 'footers', 'quotes']))
 
     pl = A2Pipeline()
+    baseline = A2Pipeline(svm.LinearSVC())
     grid = pl.grid_search_tune()
 
     tuned = grid.fit(train_ng.data, train_ng.target)
+    baseline.fit_pipeline(train_ng)
 
     tuned_pred = pl.pred(tuned, test_ng.data)
+    base_pred = baseline.pred(baseline.pipeline, test_ng.data) 
 
     print("tuned pred")
     print(classification_report(test_ng.target, tuned_pred))
     print(np.mean(tuned_pred == test_ng.target ))
+    
+    print((grid.cv_results_))
+
+    print("base pred")
+    print(classification_report(test_ng.target, base_pred))
+    print(np.mean(base_pred == test_ng.target ))
 
 
+    # IMDB
+    imdb_train = A2Pipeline.load_imdb()
+    imdb_test = A2Pipeline.load_imdb()
 
+    i_baseline = A2Pipeline(svm.LinearSVC())
+    i_pipeline = A2Pipeline()
+    imdb_grid = i_pipeline.grid_search_tune()
+    
+    i_tuned = imdb_grid.fit(imdb_train[0], imdb_train[1])
+    i_baseline.pipeline.fit(imdb_train[0], imdb_train[1])
 
+    i_tuned_pred = i_pipeline.pred(i_tuned, imdb_test[0])
+    i_base_pred = baseline.pred(baseline.pipeline,imdb_test[0])
 
+    print('imdb tuned pred')
+    print(classification_report(imdb_test[1], i_tuned_pred))
+    print((imdb_grid.cv_results_))
+
+    print("base imdb pred")
+    print(classification_report(imdb_test[1], i_tuned_pred))
 
